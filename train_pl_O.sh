58dfcfb4043354374f2cc8b99b123f3e7373609f
isaac-kang <isaac3419@gmail.com>
2026-03-23 22:22:05 +0900

    trivial

--- train_pl_O.sh
+++ train_pl_O.sh
@@ -1,2 +1,4 @@
 python train_pl.py wandb.group=Octopus wandb.name=train_pl_-on-real +use_pl_data=False trainer.max_epochs=10 trainer.devices=4 model.warmup_pct=0.15
 python train_pl.py wandb.group=Octopus wandb.name=train_pl_-on-real_pl trainer.max_epochs=10 trainer.devices=4 model.warmup_pct=0.15
+python train_pl.py wandb.group=Octopus wandb.name=train_pl_-on-real +use_pl_data=False trainer.max_epochs=10 trainer.devices=4 model.warmup_pct=0.15
+python train_pl.py wandb.group=Octopus wandb.name=train_pl_-on-real_pl trainer.max_epochs=10 trainer.devices=4 model.warmup_pct=0.15
